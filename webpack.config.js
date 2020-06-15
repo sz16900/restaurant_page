5b45a77ef3abc5190f76ea1ff88ec0875f77efdc
sz16900 <sz16900@my.bristol.ac.uk>
2020-06-15 18:02:46 -0500

    add linters

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,21 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
 };
